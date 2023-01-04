648946c5562a6c4d0a1162e4ef12530e1f4b1d3c
Vaishnavi Paliya <vaishnavipaliya11@gmail.com>
2023-01-04 13:30:39 +0530

    style: about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,50 @@
-export const About=()=>{
-    return(
-        <div>About</div>
-    )
-}
+import { mocktest } from "../../images/AboutImg";
+import { engaging } from "../../images/AboutImg";
+import { textImg } from "../../images/AboutImg";
+import { aboutExponentialImg } from "../../images/AboutImg";
+import "../../styles.css";
+import "../about/about.css";
+export const About = () => {
+  return (
+    <div>
+      <div>
+        <h4>What's inside cherry learn</h4>
+        <div className="common-flex-row justify-space-evenly align-center">
+          <div className="about-special-container">
+            <img src={textImg} />
+            <h5>Text based activities</h5>
+            <p>Interactive and engaging activities based strictly based on</p>
+          </div>
+
+          <div className="about-special-container">
+            <img src={engaging} />
+            <h5>Engaging Animated Videos</h5>
+            <p>Super-exciting visuals and carefully-crafted animations which</p>
+          </div>
+
+          <div className="about-special-container">
+            <img src={mocktest} />
+            <h5>Mock Tests</h5>
+            <p>Hundreds of practice tests designed in English and multiple</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="about-part-two commmon-flex-row">
+        <div>
+          <h4 className="text-color">Encouraging children's potential</h4>
+          <p>
+            CherryLearn has thousands of interactive and interesting activities
+            in English and Kannada based on textbooks, technology systems to
+            complement modern education, mock tests and useful quizzes and
+            grammar to boost confidence.
+          </p>
+          <button className="secondary-btn">Download</button>
+        </div>
+        <div>
+            <img src={aboutExponentialImg}/>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/images/AboutImg.js
@@ -0,0 +1,4 @@
+export const textImg="https://www.cherrilearn.com/static/media/Group1New.b5db1eff.png"
+export const engaging="https://www.cherrilearn.com/static/media/Group2.e02f5b61.png"
+export const mocktest="https://www.cherrilearn.com/static/media/Group3.8a1c922b.png"
+export const aboutExponentialImg= "https://www.cherrilearn.com/static/media/right-learning-potential-new.fe129b92.png"
